e79dd58cf3d375df9e2818db986a3cc904408e4a
sretundijr <sretundijr@gmail.com>
2017-12-06 23:33:14 -0700

    testing img path issue

--- src/pages/about-me.js
+++ src/pages/about-me.js
@@ -3,11 +3,15 @@ import React from 'react';
 // css
 import './about-me.css';
 
+import music from './find-new-music.png';
 
 const AboutMe = () => {
   return (
     <div>
       <h1 className="about-me">About Me</h1>
+      <div>
+        <img src={`${music}`} alt="logo" />
+      </div>
     </div>
   );
 };
