34bed1ac794260e7fb0304371513beafc84b77d9
sretundijr <sretundijr@gmail.com>
2017-12-06 18:30:43 -0700

    index layout component

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,38 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Link from 'gatsby-link';
 import Helmet from 'react-helmet';
 
+// css
 import './index.css';
+import './child-container.css';
 
-const Header = () => (
-  <div
-    style={{
-      background: 'blue',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          Steve Retundi Jr.
-        </Link>
-      </h1>
-    </div>
-  </div>
-);
+// components
+import Footer from '../components/footer';
+import Header from '../components/header';
 
 const TemplateWrapper = ({ children }) => (
   <div>
@@ -43,16 +19,14 @@ const TemplateWrapper = ({ children }) => (
         { name: 'keywords', content: 'sample, something' },
       ]}
     />
-    <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
+    <div>
+      <Header />
+    </div>
+    <div className="child-container">
       {children()}
+    </div>
+    <div>
+      <Footer />
     </div>
   </div>
 );
