9e54b1a3962dcb9b73904909564c8a2c3280b79b
sretundijr <sretundijr@gmail.com>
2017-12-07 12:37:28 -0700

    test setup

--- src/__tests__/index-page.js
+++ src/__tests__/index-page.js
@@ -1,14 +1,14 @@
 
 /* global describe it */
 
-// import React from 'react';
-// import { shallow } from 'enzyme';
+import React from 'react';
+import { shallow } from 'enzyme';
 
-// import IndexPage from '../pages/index';
+import IndexPage from '../pages/index';
 
 describe('testing Index page component', () => {
   it('should render without crashing', () => {
-    //     // shallow(<IndexPage />);
+    shallow(<IndexPage />);
   });
 });
 
