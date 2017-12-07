54fa53e244542215a3186ed8b6eb29e57f321c35
sretundijr <sretundijr@gmail.com>
2017-12-06 19:58:04 -0700

    checking ci integration

---
+++ src/__tests__/index-page.js
@@ -0,0 +1,14 @@
+
+/* global describe it */
+
+// import React from 'react';
+// import { shallow } from 'enzyme';
+
+// import IndexPage from '../pages/index';
+
+describe('testing Index page component', () => {
+  it('should render without crashing', () => {
+    //     // shallow(<IndexPage />);
+  });
+});
+
