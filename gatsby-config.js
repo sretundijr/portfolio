43eed807a79c5b5eae0cfc66e3b723c0471c0f7f
sretundijr <sretundijr@gmail.com>
2017-12-06 23:47:37 -0700

    resolve img path issues in production

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,7 @@
 module.exports = {
+  pathPrefix: './public',
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: 'Gatsby Default Starter',
   },
-  plugins: [`gatsby-plugin-react-helmet`],
-}
+  plugins: ['gatsby-plugin-react-helmet'],
+};
--- src/pages/about-me.js
+++ src/pages/about-me.js
@@ -3,15 +3,11 @@ import React from 'react';
 // css
 import './about-me.css';
 
-import music from './find-new-music.png';
 
 const AboutMe = () => {
   return (
     <div>
       <h1 className="about-me">About Me</h1>
-      <div>
-        <img src={`${music}`} alt="logo" />
-      </div>
     </div>
   );
 };
