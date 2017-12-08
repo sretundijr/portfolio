f453aac40c9325bb4f0ccd534c82e4f9436c6be3
sretundijr <sretundijr@gmail.com>
2017-12-07 21:51:03 -0700

    removed borders

--- src/components/card.js
+++ src/components/card.js
@@ -4,7 +4,6 @@ import React from 'react';
 import './card.css';
 
 const Card = (props) => {
-  console.log(props);
   return (
     <div className="card-container">
       <img src={props.project.image} alt="project images" />
