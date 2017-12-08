c52b0e4403d532f8981dc086ac643da8f74fca49
sretundijr <sretundijr@gmail.com>
2017-12-08 02:17:32 -0700

    about me image holders

--- src/pages/about-me.js
+++ src/pages/about-me.js
@@ -9,7 +9,12 @@ const AboutMe = () => {
   return (
     <div>
       <h2 className="about-page-title">The Journey So Far</h2>
-      <p className="about-page-content">{personalInfo().bio}</p>
+      <div className="about-page-container">
+        <div className="about-square-1 about-image-1" />
+        <div className="about-page-content">
+          <p>{personalInfo().bio}</p>
+        </div>
+      </div>
     </div>
   );
 };
