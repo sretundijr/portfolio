4c7d837b8e67860b4350426c16bc75196b28031b
sretundijr <sretundijr@gmail.com>
2017-12-08 02:18:06 -0700

    index page style

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,10 +4,15 @@ import aboutMe from '../personal-info';
 
 // css
 import './index.css';
+import './placeHolder.css';
 
 const IndexPage = () => (
   <div>
-    <p className="about">{aboutMe().about}</p>
+    <div className="index-container">
+      <p className="index-content">{aboutMe().about}</p>
+      <div className="first-image large-square" />
+    </div>
+    <div className="second-image xLarge-square" />
   </div>
 );
 
